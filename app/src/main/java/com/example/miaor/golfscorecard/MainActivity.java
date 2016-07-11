b9f46bca63e2ab59d189fa911772a97881678c87
ohmyskyhigh <miaorunkun@gmail.com>
2016-07-11 21:29:38 +0800

    V0.1
    add app bar

---
+++ app/src/main/java/com/example/miaor/golfscorecard/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.miaor.golfscorecard;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.my_toolbar)
+    Toolbar mToolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        setSupportActionBar(mToolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_item, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.clear_record){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/example/miaor/golfscorecard/ScoreRecord.java
@@ -0,0 +1,19 @@
+package com.example.miaor.golfscorecard;
+
+
+/**
+ * created by the one and only, Runkun Miao!!!!!!!!!
+ */
+public class ScoreRecord {
+    private int[] mScore = new int[18];
+
+
+    public int[] getScore() {
+        return mScore;
+    }
+
+
+    public void setScore(int[] score) {
+        mScore = score;
+    }
+}
